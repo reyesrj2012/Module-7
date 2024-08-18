3a6ced56164d2f08ec976fa2571bad9ecb8630be
reyesrj2012 <reyesrj2012@gmail.com>
2024-08-18 11:18:08 -0500

    devolop and starter code

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,34 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)';
+    // Add more cases for other licenses as needed
+    default:
+      return '';
+  }
+}
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+
+function renderLicenseLink(license) {
+return "";
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+export default generateMarkdown;
